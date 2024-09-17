de1a4f82050e13bbe09e790e96df6823f655966a
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-09-16 22:07:04 -0700

    Algorithm Implementation, Seed, Clear Buttons w/ funtionalites, Gradient UI Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,97 @@
-import { useState } from "react";
-import { COLS, ROWS, createEmptyGrid } from "./utils/utils";
+import { useCallback, useState } from "react";
+import { COLS, DIRECTIONS, ROWS, createEmptyGrid } from "./utils/utils";
 import { twMerge } from "tailwind-merge";
+import React from "react";
+import { PlayPauseButton } from "./components/PlayPauseButton";
+import { Button } from "./components/Button";
 
 function App() {
   const [grid, setGrid] = useState<number[][]>(createEmptyGrid());
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playingRef = React.useRef(isPlaying);
+  playingRef.current = isPlaying;
+
+  const runGameOfLife = useCallback(() => {
+    if (!playingRef.current) {
+      return;
+    }
+
+    setGrid((currentGrid) => {
+      const newGrid = currentGrid.map((arr) => [...arr]);
+
+      for (let row = 0; row < ROWS; row++) {
+        for (let col = 0; col < COLS; col++) {
+          let liveNeighbors = 0;
+
+          DIRECTIONS.forEach(([directionX, directionY]) => {
+            const neighborRow = row + directionX;
+            const neighborCol = col + directionY;
+
+            if (
+              neighborRow >= 0 &&
+              neighborRow < ROWS &&
+              neighborCol >= 0 &&
+              neighborCol < COLS
+            ) {
+              liveNeighbors += currentGrid[neighborRow][neighborCol] ? 1 : 0;
+            }
+          });
+
+          if (liveNeighbors < 2 || liveNeighbors > 3) {
+            newGrid[row][col] = 0;
+          } else if (currentGrid[row][col] === 0 && liveNeighbors === 3) {
+            newGrid[row][col] = 1;
+          }
+        }
+      }
+
+      return newGrid;
+    });
+
+    setTimeout(runGameOfLife, 100);
+  }, [playingRef, setGrid]);
 
   return (
     <>
-      <div className="h-screen w-screen flex justify-center p-4 bg-blue-500 flex-col gap-4">
+      <div className="h-screen w-screen flex items-center p-4  flex-col gap-4">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#a333ee_100%)]"></div>
+
         <h1 className="md:text-2xl text-xl">Conway's Game of Life</h1>
+        <div className="flex gap-4 items-center">
+          <PlayPauseButton
+            isPlaying={isPlaying}
+            onClick={() => {
+              setIsPlaying(!isPlaying);
+              if (!isPlaying) {
+                playingRef.current = true;
+                // run simulation here
+                runGameOfLife();
+              }
+            }}
+          />
+          <Button
+            onClick={() => {
+              const rows = [];
+              for (let i = 0; i < ROWS; i++) {
+                rows.push(
+                  Array.from(Array(COLS), () => (Math.random() > 0.75 ? 1 : 0))
+                );
+              }
+              setGrid(rows);
+            }}
+          >
+            Seed
+          </Button>
+          <Button
+            onClick={() => {
+              setGrid(createEmptyGrid());
+              setIsPlaying(false);
+            }}
+          >
+            Clear
+          </Button>
+        </div>
         <div
           style={{
             display: "grid",
@@ -17,15 +100,15 @@ function App() {
           }}
         >
           {grid.map((rows, originalRowIndex) =>
-            rows.map((col, originalColIndex) => (
+            rows.map((_col, originalColIndex) => (
               <button
                 key={`${originalRowIndex}-${originalColIndex}`}
-                className={
-                  (twMerge("border border-[#9050e9]"),
+                className={twMerge(
+                  "border border-[#9050e9]",
                   grid[originalRowIndex][originalColIndex]
                     ? "bg-[#ad7bee]"
-                    : "bg-[#240643]")
-                }
+                    : "bg-[#240643]"
+                )}
               />
             ))
           )}
---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export function Button({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: React.ReactNode;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className="transition ease-in flex items-center justify-center h-8 w-8 rounded-full px-10 shadow-md bg-gray-700 hover:bg-gray-800"
+    >
+      {children}
+    </button>
+  );
+}
